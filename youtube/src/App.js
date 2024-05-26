3a5f3a9b0a714ae6991ec10fab91df724becda90
Tom Sasson <toms752384@gmail.com>
2024-05-26 12:45:18 +0300

    commit

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-
+import LoginForm from './login screen/LoginForm';
+import LoginLeft from './login screen/LoginLeft'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <LoginLeft></LoginLeft>
+    <LoginForm></LoginForm>
+    </>
   );
 }
 
---
+++ youtube/src/login screen/LoginLeft.js
@@ -0,0 +1,18 @@
+function LoginLeft() {
+    return (
+        <div>
+            <logo>
+                <img src="{logo}" className="card-img-top" alt="YouTube Logo" />
+            </logo>
+            <text>
+                Sign In
+            </text>
+            <text>Not a member? Click here to Sign up!</text>
+            <button>
+                Sign Up
+            </button>
+        </div>
+    );
+}
+
+export default LoginLeft;
