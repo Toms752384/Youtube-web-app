767add8bbc3d9a4d1f23647fcd0d0ca6deaa2244
Tom Sasson <toms752384@gmail.com>
2024-05-27 22:18:47 +0300

    commit

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
-// import headerOfRegist from './Registration screen/headerOfRegist';
-// import impo from './Registration screen/impo';
+import headerOfRegist from './Registration screen/headerOfRegist';
+import impo from './Registration screen/impo';
 import LoginContainer from './login screen/LoginContainer';
 import './login screen/Login.css'
 
 function App() {
   return (
-    <LoginContainer></LoginContainer>
+    <>
+    </>
   );
 }
 
