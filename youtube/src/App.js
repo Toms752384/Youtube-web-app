577333e828e3fd4c6d1d38048ae0e0a8a0fa05b0
Tom Sasson <toms752384@gmail.com>
2024-05-27 22:54:38 +0300

    commit

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React from 'react';
-import headerOfRegist from './Registration screen/headerOfRegist';
-import impo from './Registration screen/impo';
+// import headerOfRegist from './Registration screen/headerOfRegist';
+// import impo from './Registration screen/impo';
 import LoginContainer from './login screen/LoginContainer';
 import './login screen/Login.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <>
-    </>
+    <LoginContainer></LoginContainer>
   );
 }
 
