22b940f45dad450486d8ef79c217773c56687345
Tom Sasson <toms752384@gmail.com>
2024-06-06 20:03:33 +0300

    commit

--- youtube/src/login screen/MoveToRegistration.js
+++ youtube/src/login screen/MoveToRegistration.js
@@ -11,8 +11,9 @@ function MoveToRegistration() {
         <>
             <div className="form-text mb-3 text-center">
                 Not a member yet? Sign up today!
-            </div>        <div className="form-floating mb-3 text-center" onClick={handleButtonClick}>
-                <button type="button" className="btn btn-danger">
+            </div>
+            <div className="form-floating mb-3 text-center" onClick={handleButtonClick}>
+                <button type="button" className="login-button">
                     Sign Up
                 </button>
             </div>
