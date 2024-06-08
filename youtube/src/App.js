f6670dd1c5e40b2abcc5aa023662667f3f03d2a8
Tom Sasson <toms752384@gmail.com>
2024-06-08 17:50:18 +0300

    commit

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const addUser = (newUser) => {
     setUsers([...users, newUser]);
+    console.log(users);
   };
 
   //add current user functions!
--- youtube/src/RegistrationScreen/RegistButton.js
+++ youtube/src/RegistrationScreen/RegistButton.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import confetti from 'canvas-confetti';
 import { verifyRegistration } from './validation.js';
+import { useNavigate } from 'react-router-dom';
 
 function RegistButton({ users, addUser }) {
   const handleClick = () => {
+    //first check if valid
     var valid = verifyRegistration(users);
     if (valid) {
       const newUser = createNewUser();
+      //check if unique
       if (isUniqueUser(newUser)) {
+        //add user, celebrate and go to sign up
         addUser(newUser);
         confetti();
+        navigateToLogin();
       }
     };
   };
 
+  //function to create new user
   const createNewUser = () => {
     var username = document.getElementById("username").value;
     var password = document.getElementById("floatingPassword").value;
     var nickname = document.getElementById("nickname").value;
-    return { username, password, nickname };
+    var avatar = document.getElementById("upload-input").value;
+    return { username, password, nickname, avatar };
   };
 
+  //fuction to check if new user is unique in list
   const isUniqueUser = (newUser) => {
     for (let user of users) {
       if (user.username === newUser.username) {
@@ -38,6 +46,12 @@ function RegistButton({ users, addUser }) {
     }
     return true;
   };
+
+  //function to navigate to login page
+  const navigate = useNavigate();
+  const navigateToLogin = () => {
+  navigate('/');
+    };
 
   return (
     <div className="Dubi">
