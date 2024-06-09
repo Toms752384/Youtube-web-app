06c1a374b420d28dda8a24c2dd6b30ca9d8d3253
Tom Sasson <toms752384@gmail.com>
2024-06-09 23:46:34 +0300

    commit

--- youtube/src/Watching a video/SideBar.js
+++ youtube/src/Watching a video/SideBar.js
@@ -1,24 +1,33 @@
+import React from 'react';
+import './SideBar.css';
+
 function SideBar() {
     return (
         <div className="col-md-4">
             <div className="sidebar">
-                <div className="video-item mb-3">
+                <div className="video-item mb-3 d-flex">
                     <img src="https://i.ytimg.com/vi/zxDUpSP1ZGo/hqdefault.jpg" alt="Video Thumbnail" className="img-fluid" />
-                    <div className="video-title mt-2">Artist</div>
-                    <div className="video-title mt-2">Video1</div>
-                    <div>16M views • 1 year ago</div>
+                    <div className="video-info ml-3">
+                        <div className="video-title">Artist</div>
+                        <div className="video-title">Video1</div>
+                        <div>16M views • 1 year ago</div>
+                    </div>
                 </div>
-                <div className="video-item mb-3">
+                <div className="video-item mb-3 d-flex">
                     <img src="https://i.ytimg.com/vi/3x8hDkp_bT0/hqdefault.jpg" alt="Video Thumbnail" className="img-fluid" />
-                    <div className="video-title mt-2">Artist</div>
-                    <div className="video-title mt-2">Video2</div>
-                    <div>16M views</div>
+                    <div className="video-info ml-3">
+                        <div className="video-title">Artist</div>
+                        <div className="video-title">Video2</div>
+                        <div>16M views</div>
+                    </div>
                 </div>
-                <div className="video-item mb-3">
+                <div className="video-item mb-3 d-flex">
                     <img src="https://i.ytimg.com/vi/4_g19AdGZ5M/hqdefault.jpg" alt="Video Thumbnail" className="img-fluid" />
-                    <div className="video-title mt-2">Artist</div>
-                    <div className="video-title mt-2">Video3</div>
-                    <div>11M views</div>
+                    <div className="video-info ml-3">
+                        <div className="video-title">Artist</div>
+                        <div className="video-title">Video3</div>
+                        <div>11M views</div>
+                    </div>
                 </div>
             </div>
         </div>
