501b865c55743b17d96f0aa1d02bb2a941c309bb
Tom Sasson <toms752384@gmail.com>
2024-06-14 20:54:56 +0300

    commit

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import LoginContainer from './login screen/LoginContainer.js';
 import RegistrationContainer from './RegistrationScreen/RegistrationContainer.js';
