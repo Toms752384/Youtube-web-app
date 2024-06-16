3ae5c2fee76adbbb097c10b61bb2b0942446419c
Tom Sasson <toms752384@gmail.com>
2024-06-16 11:50:24 +0300

    commit

--- youtube/src/Watching a video/IconsLeft.js
+++ youtube/src/Watching a video/IconsLeft.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Video.css';
 import FloatingMenu from './FloatingMenu';
 import logo from './logo.png';
 
-function IconsLeft({currentUser, handleSignOut, defualtUser}) {
+function IconsLeft({ currentUser, handleSignOut, defualtUser }) {
     //floating menu state
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleMenuToggle = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    //function to navigate to home page
+    const navigate = useNavigate();
+    const handleMoveToHome = () => {
+        navigate('/home');
+    }
     return (
         <div className="d-flex align-items-center">
             <i
@@ -18,8 +25,8 @@ function IconsLeft({currentUser, handleSignOut, defualtUser}) {
                 style={{ fontSize: '1.5rem', cursor: 'pointer' }}
                 onClick={handleMenuToggle}
             ></i>
-            <img src={logo} alt="YouTube Logo" />
-            <FloatingMenu isOpen={isMenuOpen} onClose={handleMenuToggle} currentUser={currentUser} defualtUser={defualtUser} handleSignOut={handleSignOut}/>
+            <img src={logo} alt="YouTube Logo" className="left-logo" onClick={handleMoveToHome}/>
+            <FloatingMenu isOpen={isMenuOpen} onClose={handleMenuToggle} currentUser={currentUser} defualtUser={defualtUser} handleSignOut={handleSignOut} />
         </div>
     );
 }
--- youtube/src/add video screen/AddVideoLogo.js
+++ youtube/src/add video screen/AddVideoLogo.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.png';
+import { useNavigate } from 'react-router-dom';
 
 function AddVideoLogo() {
+  //function to navigate to home page
+  const navigate = useNavigate();
+  const handleMoveToHome = () => {
+    navigate('/home');
+  }
+
   return (
-    // move to home page
-      <img src={logo} alt="logo" className="my-image" />
+    <img src={logo} alt="logo" className="my-image" onClick={handleMoveToHome}/>
   );
 }
 
